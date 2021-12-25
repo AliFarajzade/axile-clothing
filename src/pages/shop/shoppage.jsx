e7655a7e2c1c224005559b4bcf8a112430eef3d2
AliFarajzade <alifarajzade1@yahoo.com>
2021-12-25 03:11:17 -0500

    Handling Shop Data & Directory Data with Redux.

--- src/pages/shop/shoppage.jsx
+++ src/pages/shop/shoppage.jsx
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import { SHOP_DATA } from './shopData';
+import React from 'react';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import { selectShopCollections } from '../../redux/shop/shop.selectors';
+import { connect } from 'react-redux';
 
-export default class ShopPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            collections: SHOP_DATA,
-        };
-    }
-
-    render() {
-        return (
-            <div className="shop-page">
-                {this.state.collections.map(
-                    ({ id, ...otherShopDataProperties }) => (
-                        <CollectionPreview
-                            key={id}
-                            {...otherShopDataProperties}
-                        />
-                    )
-                )}
-            </div>
-        );
-    }
+function ShopPage({ selectShopCollections }) {
+    return (
+        <div className="shop-page">
+            {selectShopCollections.map(({ id, ...otherShopDataProperties }) => (
+                <CollectionPreview key={id} {...otherShopDataProperties} />
+            ))}
+        </div>
+    );
 }
+
+const mapStateToProps = state => ({
+    selectShopCollections: selectShopCollections(state),
+});
+
+export default connect(mapStateToProps)(ShopPage);
