568dade4ad0cd5ce9921c10053c1a2f4310f84ba
AliFarajzade <alifarajzade1@yahoo.com>
2021-12-26 08:50:27 -0500

    Adding Styled Components To Our Home Page and Header.

--- src/pages/home/hompage.jsx
+++ src/pages/home/hompage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import DirectoryMenu from '../../components/directory-menu/directory-menu.component';
 
+import { HomePageContainer } from './homepage.styles';
+
 export default function HomePage() {
     return (
-        <div className="homepage">
+        <HomePageContainer>
             <DirectoryMenu />
-        </div>
+        </HomePageContainer>
     );
 }
